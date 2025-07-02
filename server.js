7161e82cd09bf013a875a408a57af833d9eafe2f
Monire En <monire.eshghi@gmail.com>
2025-07-02 11:17:21 -0400

    Update the package

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 5000;
-
+server.use(middlewares);
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-
-server.use(middlewares);
 server.use(router);
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
